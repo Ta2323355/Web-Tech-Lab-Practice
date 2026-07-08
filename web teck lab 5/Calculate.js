68ceac938d5f5558dabd3c04ee2edecf6f5aa622
Ta2323355 <tanuchakraborty1823@gmail.com>
2026-07-08 11:59:23 +0600

    lab 5

---
+++ web teck lab 5/Calculate.js
@@ -0,0 +1,16 @@
+function calculateAmount() {
+  var price = Number(document.getElementById("productPrice").value);
+  var quantity = document.getElementById("quantity").value;
+  var errorMsg = document.getElementById("errorMsg");
+  var payableAmount = document.getElementById("payableAmount");
+
+  quantity = Number(quantity);
+
+  if (quantity <= 0 || isNaN(quantity) || document.getElementById("quantity").value === "") {
+    errorMsg.innerHTML = "Quantity must be greater than 0";
+    payableAmount.value = 0;
+  } else {
+    errorMsg.innerHTML = "";
+    payableAmount.value = price * quantity;
+  }
+}
